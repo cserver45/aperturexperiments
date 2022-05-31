bdaeb897afa0a21c440298c6a2943f449bd6a1a2
cserver45 <77689658+cserver45@users.noreply.github.com>
2022-05-31 17:09:08 -0400

    moved 8ball possible results into its own file. (games.py)

--- cogs/games.py
+++ cogs/games.py
@@ -130,26 +130,8 @@ class Games(Cog):
     @command(name="8ball", aliases=['eightball'])
     async def _8ball(self, ctx: Context, *, question: str) -> None:
         """Its an 8ball. That's it."""
-        responses = ['It is certain.',
-                     'It is decidedly so.',
-                     'Without a doubt.',
-                     'Yes – definitely.',
-                     'You may rely on it.',
-                     'As I see it, yes.',
-                     'Most likely.',
-                     'Outlook good.',
-                     'Yes.',
-                     'Signs point to yes.',
-                     'Reply hazy, try again.',
-                     'Ask again later.',
-                     'Better not tell you now.',
-                     'Cannot predict now.',
-                     'Concentrate and ask again.',
-                     'Don\'t count on it.',
-                     'My reply is no.',
-                     'My sources say no.',
-                     'Outlook not so good.',
-                     'Very doubtful.']
+        async with aiofiles.open("8ball.json", mode="r") as f:
+            responses = orjson.loads(await f.read())
         await ctx.send(f'Question: {question}\nAnswer: {choice(responses)}')
 
     @command()
