ebf7ef33751b95cc72bb05d2b94b5292afa97b0d
cserver45 <77689658+cserver45@users.noreply.github.com>
2023-11-06 10:48:14 -0500

    remove all dead endpoints in `animals.py` (#2)

--- cogs/animals.py
+++ cogs/animals.py
@@ -31,14 +31,6 @@ class Animals(Cog):
 
     @cooldown(1, 5, BucketType.user)
     @command()
-    async def cat(self, ctx: Context) -> None:
-        """Get a random cat photo/gif."""
-        async with self.session.get('https://aws.random.cat/meow') as resp:
-            catresult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Cat Photo link: {catresult["file"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command()
     async def fox(self, ctx: Context) -> None:
         """Get a random fox photo/gif."""
         async with self.session.get('https://randomfox.ca/floof/') as resp:
@@ -60,66 +52,6 @@ class Animals(Cog):
         async with self.session.get('https://random-d.uk/api/v2/random') as resp:
             duckresult = await resp.json(loads=orjson.loads)
         await ctx.send(f'Duck photo link: {duckresult["url"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command()
-    async def bird(self, ctx: Context) -> None:
-        """Get a random bird photo/gif."""
-        async with self.session.get('https://some-random-api.ml/img/birb') as resp:
-            birdresult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Bird Photo link: {birdresult["link"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command()
-    async def kangaroo(self, ctx: Context) -> None:
-        """Get a random kangaroo photo/gif."""
-        async with self.session.get('https://some-random-api.ml/img/kangaroo') as resp:
-            kresult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Kangaroo Photo link: {kresult["link"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command()
-    async def koala(self, ctx: Context) -> None:
-        """Get a random koala photo/gif."""
-        async with self.session.get('https://some-random-api.ml/img/koala') as resp:
-            koresult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Koala Photo link: {koresult["link"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command()
-    async def red_panda(self, ctx: Context) -> None:
-        """Get a random red panda photo/gif."""
-        async with self.session.get('https://some-random-api.ml/img/red_panda') as resp:
-            rresult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Red panda Photo link: {rresult["link"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command()
-    async def panda(self, ctx: Context) -> None:
-        """Get a random panda photo/gif."""
-        async with self.session.get('https://some-random-api.ml/img/panda') as resp:
-            presult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Panda Photo link: {presult["link"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command()
-    async def whale(self, ctx: Context) -> None:
-        """Get a random whale photo/gif."""
-        async with self.session.get('https://some-random-api.ml/img/whale') as resp:
-            wresult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Whale Photo link: {wresult["link"]}')
-
-    @cooldown(1, 5, BucketType.user)
-    @command(hidden=True)
-    async def lizard(self, ctx: Context) -> None:
-        """Get a random lizard photo/gif.
-
-        this is an undocumented endpoint for this api,
-        so this command could fail at any time.
-        """
-        async with self.session.get('https://nekos.life/api/v2/img/lizard') as resp:
-            lresult = await resp.json(loads=orjson.loads)
-        await ctx.send(f'Lizard photo link: {lresult["url"]}')
 
 
 def setup(bot: Client) -> None:
